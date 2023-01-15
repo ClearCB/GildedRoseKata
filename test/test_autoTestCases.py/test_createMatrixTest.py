fe7cfe33ff052b126f8c3f2dd143d7ec95a4a1bf
ClearCB <abelcasasccb@gmail.com>
2023-01-15 22:08:01 +0100

    test(createMatrixTest): add test to method createMatrixTest

--- test/test_autoTestCases.py/test_createMatrixTest.py
+++ test/test_autoTestCases.py/test_createMatrixTest.py
@@ -1,5 +1,5 @@
 from src.autoTestCases.createMatrixTest import createMatrixTest
-from test_variables import first_day_items, first_day_array
+from test_variables import items, first_day_array, second_day_array, matrix_items
 import pytest
 
 '''
@@ -9,4 +9,7 @@ This module will test the createMatrixTest rutines to get the matrix correctly
 @pytest.mark.test_createMatrixTest
 def test_createMatrixTest():
 
-    assert createMatrixTest(first_day_items) == first_day_array
+    assert createMatrixTest(items)[0] == first_day_array
+    assert createMatrixTest(items)[1] == second_day_array
+    assert createMatrixTest(items) == first_day_array
+    assert createMatrixTest(items) == matrix_items
--- test/test_autoTestCases.py/test_variables.py
+++ test/test_autoTestCases.py/test_variables.py
@@ -1,4 +1,4 @@
-first_day_items = '''
+items = '''
 -------- day 0 --------
 name, sellIn, quality
 +5 Dexterity Vest, 10, 20
@@ -11,16 +11,42 @@ Backstage passes to a TAFKAL80ETC concert, 10, 49
 Backstage passes to a TAFKAL80ETC concert, 5, 49
 Conjured Mana Cake, 3, 6
 
+-------- day 1 --------
+name, sellIn, quality
++5 Dexterity Vest, 9, 18
+Aged Brie, 1, 1
+Elixir of the Mongoose, 4, 6
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 14, 21
+Backstage passes to a TAFKAL80ETC concert, 9, 50
+Backstage passes to a TAFKAL80ETC concert, 4, 50
+Conjured Mana Cake, 2, 4
+
 '''
 
-first_day_array = [
-                ["+5 Dexterity Vest",10,20],
-                ["Aged Brie",2,0],
-                ["Elixir of the Mongoose", 5, 7],
-                ["Sulfuras, Hand of Ragnaros", 0, 80],
-                ["Sulfuras, Hand of Ragnaros", -1, 80],
-                ["Backstage passes to a TAFKAL80ETC concert", 15, 20],
-                ["Backstage passes to a TAFKAL80ETC concert", 10, 49],
-                ["Backstage passes to a TAFKAL80ETC concert", 5, 49],
-                ["Conjured Mana Cake", 3, 6]
-                ]
+first_day_array =   [
+                    ["+5 Dexterity Vest",10,20],
+                    ["Aged Brie",2,0],
+                    ["Elixir of the Mongoose", 5, 7],
+                    ["Sulfuras, Hand of Ragnaros", 0, 80],
+                    ["Sulfuras, Hand of Ragnaros", -1, 80],
+                    ["Backstage passes to a TAFKAL80ETC concert", 15, 20],
+                    ["Backstage passes to a TAFKAL80ETC concert", 10, 49],
+                    ["Backstage passes to a TAFKAL80ETC concert", 5, 49],
+                    ["Conjured Mana Cake", 3, 6]
+                    ]
+
+second_day_array =  [
+                    ["+5 Dexterity Vest", 9, 18],
+                    ["Aged Brie", 1, 1],
+                    ["Elixir of the Mongoose", 4, 6],
+                    ["Sulfuras, Hand of Ragnaros", 0, 80],
+                    ["Sulfuras, Hand of Ragnaros", -1, 80],
+                    ["Backstage passes to a TAFKAL80ETC concert", 14, 21],
+                    ["Backstage passes to a TAFKAL80ETC concert", 9, 50],
+                    ["Backstage passes to a TAFKAL80ETC concert", 4, 50],
+                    ["Conjured Mana Cake", 2, 4]
+                    ]
+
+matrix_items = [[first_day_array]+[second_day_array]]
