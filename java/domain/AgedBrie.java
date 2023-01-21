5a96f4743b387507fc70fcd4d798cdcf122b6aa7
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-01-21 23:10:41 +0100

    feat(AgedBrie): add class NormalItem

--- java/domain/AgedBrie.java
+++ java/domain/AgedBrie.java
@@ -1,5 +1,13 @@
 package java.domain;
 
-public class AgedBrie {
+public class AgedBrie extends NormalItem  {
     
+    public AgedBrie(String name, int sellIn, int quality){
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality(){
+        ;
+    }
 }
