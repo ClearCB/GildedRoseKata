fee698f69041f398ab9359579d8e7cc34c37e628
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-01-22 13:19:41 +0100

    feat(Sulfuras): new item sulfuras

--- java/domain/Sulfuras.java
+++ java/domain/Sulfuras.java
@@ -1,5 +1,16 @@
 package java.domain;
 
-public class Sulfuras {
+public class Sulfuras extends NormalItem{
     
+
+    public Sulfuras(String name, int sellIn, int quality){
+    
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality(){
+
+        this.setQuality(80);
+    }
 }
