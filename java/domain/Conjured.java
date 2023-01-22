9aab73512449ee108d419e710713a82cc22edf67
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-01-22 13:13:47 +0100

    feat(Conjured): add new conjured quality behaviour

--- java/domain/Conjured.java
+++ java/domain/Conjured.java
@@ -1,5 +1,28 @@
 package java.domain;
 
-public class Conjured {
+public class Conjured extends NormalItem{
     
+
+    public Conjured(String name, int sellIn, int quality){
+
+        super(name, sellIn, quality);
+
+    }
+
+    @Override
+    public void updateQuality(){
+
+        if (this.getSellIn()>=0){
+
+            this.calculateQuality(-2);
+        }
+
+        else if (this.getSellIn()<0){
+
+            this.calculateQuality(-4);
+        }
+
+        this.setSellIn();
+    }
+
 }
